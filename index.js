1d91e4d394c68bd6744655b44f79e7e1735f8b6a
vansulmaca07 <153063383+vansulmaca07@users.noreply.github.com>
2024-01-11 08:50:43 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var employeeid= [];
 var errorid= [];
+var machineid=[];
 var index= [];
 var maintenanceid= [];
 var ackid= [];
@@ -15,17 +16,15 @@ var lotid= [];
 var lotid2= [];
 var time1st= [];
 var totaltime =[];
-//var startTime =[];
 var endTime=[];
 var starT=[];
 var time3=[];
 var errorencountered=[];
+var tr4=[];
+var trial=[];
 
-//var errorencountered=document.getElementById('errorid').value;
 function firstinput(){
-
-    //document.getElementById('StatusTable').style.display="block";
-  
+   
     var sub11= document.getElementById('employeeid').value;    
     let errorencountered=document.getElementById('errorid').value;
     if (errorencountered==='Other'){
@@ -34,14 +33,13 @@ function firstinput(){
     else {
       var sub22=document.getElementById('errorid').value;
     }
-   // var sub22= document.getElementById('errorid').value;
+   
     var sub33= document.getElementById('lotid').value;
     const sub55= new Date();  
     starT=new Date().getTime();
-    //const startTime= new Date().getTime();
+    var sub69=document.getElementById('machineid').value;
 
-
-    if(sub11&&sub22 !=='')
+    if(sub11&&sub22&&sub69 !=='')
     {
 
       employeeid= [...employeeid,sub11];
@@ -49,13 +47,6 @@ function firstinput(){
       time1= [...time1,sub55];
       index1= [...index1,g];  
       lotid= [...lotid,sub33];
-      //starT= [...starT,startTime];
-
- //     maintenanceid= [...maintenanceid,sub3];
- //     actiondone= [...actiondone,sub7];
- //     ackid= [...ackid,sub4];    
-  //    time2= [...time2,sub6];
-  //    totaltime= [...totaltime,sub8];
   
     displayTable1();
 
@@ -64,41 +55,43 @@ function firstinput(){
         alert("入力不完全(Input Incomplete)");
 }
 
+function TestLang(){
+
+trial=document.getElementById('maintenanceid').value;
+var ad77=document.createElement('td');
+ad77.innerHTML = trial;
+tr4.appendChild(ad77);
+
+document.getElementById('status').appendChild(tr4);
+}
+
 function displayTable1(){  
   const length= employeeid.length;
-  var tr=document.createElement('tr');
+  tr4=document.createElement('tr');
 
   for(var i=0;i<length;i++)
   {
-        tr.innerHTML='';
-        //var ad1=document.createElement('td');
-        //ad1.innerHTML= index1[i];
-        //tr.appendChild(ad1);
-
+        tr4.innerHTML='';
+      
         var ad33=document.createElement('td');
         ad33.innerHTML= lotid[i];
-        tr.appendChild(ad33);
+        tr4.appendChild(ad33);
 
         var ad5=document.createElement('td');
         ad5.innerHTML= time1[i];
-        tr.appendChild(ad5);
+        tr4.appendChild(ad5);
            
         var ad2=document.createElement('td');
         ad2.innerHTML= employeeid[i];
-        tr.appendChild(ad2);
+        tr4.appendChild(ad2);
 
         var ad3=document.createElement('td');
         ad3.style.backgroundColor = "red";
         ad3.style.color= "black";
         ad3.innerHTML= errorid[i];
-        tr.appendChild(ad3);
+        tr4.appendChild(ad3);
 
-        //var ad99=starT[i];
-        //var ad99=document.createElement('td');
-        //ad99.innerHTML= starT[i];
-        //tr.appendChild(ad99);
-
-        document.getElementById('status').appendChild(tr);  
+        document.getElementById('status').appendChild(tr4);  
   }
   g++;
 
@@ -117,7 +110,6 @@ function addToTable(){
    
   document.getElementById('no-data').style.display="none";
   var sub1= document.getElementById('employeeid').value;
-  //var sub2= document.getElementById('errorid').value;
 
   let errorencountered=document.getElementById('errorid').value;
   if (errorencountered==='Other'){
@@ -131,24 +123,23 @@ function addToTable(){
   var sub4= document.getElementById('ackid').value;
   var sub7= document.getElementById('actiondone').value;
   var sub33 =document.getElementById('lotid').value;
+  var sub69 =document.getElementById('machineid').value;
+
   const sub6= new Date().getTime();
   const sub68= new Date();
   var sub77 = ((sub6-starT)/1000)/60;
   sub77 = sub77.toFixed(2);  
-  //totaltime=sub6-starT;
-  //const totaltime=(startTime-end)/1000;
-  //const sub77=end-start;
+ 
   if(sub1&&sub2&&sub3&&sub4 !=='')
 
   {
+  machineid= [...machineid,sub69];
   employeeid2= [...employeeid2,sub1];
   errorid2= [...errorid2,sub2];
   maintenanceid= [...maintenanceid,sub3];
   actiondone= [...actiondone,sub7];
   lotid2= [...lotid2,sub33];
   ackid= [...ackid,sub4];
-  //time1st= [...time1st,sub5];
-  //time2= [...time2,sub6];
   time3= [...time3,sub68];
   index= [...index,c];
   totaltime=[...totaltime,sub77]; 
@@ -176,7 +167,7 @@ function displayTable(){
         tr.appendChild(td1);
 
         var td89=document.createElement('td');
-        td89.innerHTML= "SW39";
+        td89.innerHTML= machineid[i];
         tr.appendChild(td89);
 
         var td33=document.createElement('td');
@@ -211,15 +202,9 @@ function displayTable(){
         td8.innerHTML= time3[i];
         tr.appendChild(td8);
 
-        //totaltime = (time1-time2);
         var td9=document.createElement('td');
         td9.innerHTML=totaltime[i];
         tr.appendChild(td9);
-
-       //var ad90=time3[i];
-       // var td9=document.createElement('td');
-       // td9.innerHTML= totaltime[i];
-       // tr.appendChild(td9);
 
         document.getElementById('main').appendChild(tr);
     }
@@ -230,9 +215,7 @@ function displayTable(){
     document.getElementById('maintenanceid').value='';
     document.getElementById('ackid').value='';
     document.getElementById('actiondone').value='';
-    //document.getElementById('time1').value='';
     document.getElementById('lotid').value='';
-    //document.getElementById('time2').value='';
     document.getElementById('div1').innerHTML='';
 
     document.getElementById("submit-button2").disabled = false;
@@ -247,28 +230,9 @@ function displayTable(){
 
 function myDeleteFunction() {
   document.getElementById("StatusTable").deleteRow(1);
-  //document.getElementById('StatusTable').style.display="none";
 }
 
 function showfield(name){
   if(name=='Other')document.getElementById('div1').innerHTML='その他: <input type="text" name="other" value="" id="others123" placeholder="ご指定ください" >';
   else document.getElementById('div1').innerHTML='';
 }
-
-//document.getElementById('maintenanceid').value='';
-//document.getElementById('ackid').value='';
-//function save_data(){
-    //let myTable = []
-    //('.form-control').each(function() { myTable.push( $(this).val() )});
-    //localStorage.setItem('myTable', JSON.stringify(myTable));
-//}
-//$('tbody').on("DOMSubtreeModified", function() {save_data()});
-  //  $('tbody').on('change', '.form-control', function() {save_data()});
-//function load_data(){
-  //  let myTable = JSON.parse(localStorage.getItem('myTable'));
-    //for (i = 1; i < myTable.length/3; i++) $("#addrow").click()
-    //$('.form-control').each(function(index) {$(this).val(myTable[index])});
-//}
-//function refresh(){
-  //  location.reload();
-//}
