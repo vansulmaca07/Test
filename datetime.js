315aaf77fc99e8eb135f8a8eda45d1a4af5fa781
vansulmaca07 <153063383+vansulmaca07@users.noreply.github.com>
2024-01-11 08:49:15 +0900

    Update datetime.js

--- datetime.js
+++ datetime.js
@@ -1,2 +1,15 @@
-const d = new Date();
-document.getElementById("datenow").innerHTML = d;
+//const d = new Date();
+//document.getElementById("datenow").innerHTML = d;
+
+function updateDateTime() {
+    // create a new `Date` object
+    const now = new Date();
+
+    // get the current date and time as a string
+    const currentDateTime = now.toLocaleString();
+
+    // update the `textContent` property of the `span` element with the `id` of `datetime`
+    document.querySelector('#datenow').textContent = currentDateTime;
+  }
+
+  // call the `updateDateTime` functio
