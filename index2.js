3651128b40e5547b4ba74768a6a05adae5d45836
vansulmaca07 <153063383+vansulmaca07@users.noreply.github.com>
2024-01-17 17:09:28 +0900

    Create index2.js

---
+++ index2.js
@@ -0,0 +1,6 @@
+
+var errorData = document.getElementById('errorid');
+errorData.innerHTML = localStorage.getItem('erroridlocal');
+
+var machineData = document.getElementById('machineid');
+machineData.innerHTML = localStorage.getItem('machineidlocal');
